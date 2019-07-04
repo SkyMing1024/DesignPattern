5296e27c35e991eac22919965de9e7b5ceeabfe7
skyming <skyming1126@gmail.com>
2019-07-04 22:51:21 +0800

    add bmw

---
+++ source-code/src/skyming/designpatterns/fAbstractFactory/BmwFactory.java
@@ -0,0 +1,8 @@
+package skyming.designpatterns.fAbstractFactory;
+
+/**
+ * Author : ming.tian
+ * Date : 23:21 2019/7/3
+ */
+public class BmwFactory {
+}
---
+++ source-code/src/skyming/designpatterns/fAbstractFactory/Client.java
@@ -0,0 +1,8 @@
+package skyming.designpatterns.fAbstractFactory;
+
+/**
+ * Author : ming.tian
+ * Date : 22:36 2019/7/4
+ */
+public class Client {
+}
